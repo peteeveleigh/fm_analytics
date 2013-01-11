2742af2b228d23899978f3182f98c51ec6a90272
Pete Eveleigh <pete@fantasticmachine.co.uk>
2013-01-11 15:34:08 +0000

    Change to admin settings to prevent widget showing in apps menu
    Changed boolean from "false" to "true". This prevents the widget
    showing as an app and thus appearing the apps menu.

--- admin.php
+++ admin.php
@@ -1,6 +1,6 @@
 <?php
 if ($CurrentUser->logged_in()) {
-  $this->register_app('fm_analytics', 'Google Analytics', 5, 'Provides a basic analytics report on the dashboard', '0.5', false);
+  $this->register_app('fm_analytics', 'Google Analytics', 5, 'Provides a basic analytics report on the dashboard', '0.5', true);
   $this->require_version('fm_analytics', '2.0.8');
   $this->add_setting('fm_analytics_ooid', 'OOcharts ID', 'text', '');
   $this->add_setting('fm_analytics_gaid', 'Google Analytics Profile ID', 'text', '');
