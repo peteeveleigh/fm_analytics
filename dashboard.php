7f5cb8955629ec39d3fbfa1cd89b3c1cb63e5b3e
Pete Eveleigh <pete@fantasticmachine.co.uk>
2013-01-12 13:47:32 +0000

    Fixed stuff!

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,28 @@ $Settings = $API->get('Settings');
 ?>
 
 
+<div class="widget">
+  <h2><?php echo $Lang->get('Site Visitor Stats'); ?></h2>
+ 	<form id="dateForm" method="post" action="#">
+  		<fieldset id="daterange">
+  			<label for="dateFrom">From <input type="text" id="dateFrom" placeholder="mm/dd/yyyy"></label>
+  			<label for="dateTo">To <input type="text" id="dateTo" placeholder="mm/dd/yyyy"></label>
+  			<input class="button" type="submit" value="Refresh">
+  		</fieldset>
+	</form>
+	<div class="bd">
+		<ul>
+			<li class="metric"><span style="font-size:1.2em;font-weight:bold;" id="pageviews"></span> Page Views</li>
+			<li class="metric"><span style="font-size:1.2em;font-weight:bold;" id="visitors"></span> Visitors</li>
+			<li class="metric"><span style="font-size:1.2em;font-weight:bold;" id="bounces"></span> Bounces</li>
+		</ul>	
+	</div>	
+</div>
+
+
+
+
+
 
 
 <script src="/controlpanel/addons/apps/fm_analytics/js/oocharts.js" type="text/javascript"></script>
@@ -14,17 +36,30 @@ $Settings = $API->get('Settings');
 		
 	//Set your ooid
 	oo.setOOId("<?php echo $Settings->get('fm_analytics_ooid')->settingValue(); ?>");
-
 	var aid 		= 	"<?php echo $Settings->get('fm_analytics_gaid')->settingValue(); ?>";
-	var dateFrom 	= 	new Date($('#dateFrom').val());
-	var dateTo 		= 	new Date($('#dateTo').val());
+
+	var dateFrom 	= 	new Date();
+	var dateTo 		= 	new Date();
 	
-	//load reqs
+
 	oo.load(function()
 	{
-		var dateFrom 	= 	new Date($('#dateFrom').val());
-		var dateTo 		= 	new Date($('#dateTo').val());
+		var dateFrom 	= 	new Date();
+		var dateTo 		= 	new Date();
 		showStats(dateFrom, dateTo);
+
+	
+		//Set initial date ranges
+		//$('#dateFrom').val(dateFrom);
+		//$('#dateTo').val(dateTo);
+
+
+		$('#dateForm').on('submit',function(){
+			var df = $('#dateFrom').val();
+			var dt = $('#dateTo').val();
+			showStats(df,dt);
+			return false;
+		})
 	});
 
 	function showStats(df, dt){
@@ -46,38 +81,5 @@ $Settings = $API->get('Settings');
 
 	}
 
-	$(function(){
-		var dateFrom = new Date();
-		var dateTo = new Date();
-		//Set initial date ranges
-		$('#dateFrom').val(dateFrom);
-		$('#dateTo').val(dateTo);
-
-
-		$('#dateForm').on('submit',function(){
-			var df = $('#dateFrom').val();
-			var dt = $('#dateTo').val();
-			showStats(df,dt);
-			return false;
-		})
-	})
+	
 </script>
-
-<div class="widget">
-  <h2>Experimental <?php echo $Lang->get('Site Stats'); ?></h2>
- 	<form id="dateForm" method="post" action="#">
-  		<fieldset id="daterange">
-  			Date range: 
-  			<input type="text" id="dateFrom" placeholder="mm/dd/yyyy"> - 
-  			<input type="text" id="dateTo" placeholder="mm/dd/yyyy">
-  			<input class="button" type="submit" value="Refresh">
-  		</fieldset>
-	</form>
-	<div class="bd">
-		<ul>
-			<li><span id="pageviews"></span> Page Views</li>
-			<li><span id="visitors"></span> Visitors</li>
-			<li><span id="bounces"></span> Bounces</li>
-		</ul>	
-	</div>	
-</div>
